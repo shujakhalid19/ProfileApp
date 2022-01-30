da79e1f42b3e7799124a8d7776c879415a1f5754
shuja <shujakhalid26@gmail.com>
2022-01-30 17:50:59 +0530

    first commit

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const s=StyleSheet.create({
+    fl1:{
+        flex:1
+    },
+    jCenter:{
+        justifyContent:'center',
+    },
+    alignitemCenter:{
+        alignItems: "center"
+    },
+    title:{
+        fontSize:32,
+    },
+    mdt10:{
+        marginTop:10
+    },
+})
+
+export default s;
